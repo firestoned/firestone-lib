05dfea470d1b5e66c34128aeaba0e47377d0e1b1
Erick Bourgeois <erick@jeb.ca>
2022-10-29 21:59:25 -0400

    Initial commit with barebones lib for firestone

---
+++ firestone_lib/cli.py
@@ -0,0 +1,64 @@
+"""
+Common CLI utility functions for firestone and firestone apps.
+"""
+import configparser
+import logging
+import logging.config
+import time
+
+import click
+import pkg_resources
+
+
+def init_logging(pkg: str, name: str) -> None:
+    """Initialize a logging config file for logging.
+
+    :param str pkg: the package name where the logging config is located.
+    :param str name: the name of the file to load for logging.
+    """
+    log_conf = pkg_resources.resource_stream(pkg, name)
+    print(f"log_conf: {log_conf}")
+    print(f"log_conf.name: {log_conf.name}")
+
+    try:
+        logging.config.fileConfig(log_conf.name)
+        logging.Formatter.converter = time.gmtime
+    except configparser.Error:
+        click.echo(f"Error parsing logging config file {log_conf.name}")
+
+
+class CommaDelimitedList(click.ParamType):
+    """A custom click parameter type tha takes coma delimited list of items."""
+
+    name = "Comma delimited list"
+
+    def __init__(self, item_type=click.STRING):
+        """Constructor for a new comma delimited list."""
+        self.item_type = item_type
+
+    # pylint: disable=inconsistent-return-statements
+    def convert(self, value: str, param: str, ctx):
+        """Convert this param value to a list, given the str."""
+        if value == "-":
+            return "-"
+
+        if value is None:
+            return []
+
+        if isinstance(value, list):
+            return value
+
+        try:
+            return [self.item_type.convert(item, param, ctx) for item in value.split(",")]
+        except AttributeError:
+            self.fail(f"{value} is not comma-delimited", param, ctx)
+
+
+IntList = CommaDelimitedList(item_type=click.INT)
+
+PathList = CommaDelimitedList(item_type=click.Path(exists=True))
+
+StrList = CommaDelimitedList()
+
+
+__all__ = ["init_logging", "IntList", "PathList", "StrList"]
---
+++ test/test_cli.py
@@ -0,0 +1,24 @@
+"""
+Test the firestone_lib.cli module.
+"""
+
+import unittest
+from unittest import mock
+
+from firestone_lib import cli
+
+
+class CliTest(unittest.TestCase):
+
+    def test_init_logger_bad_filename(self):
+        """Test firestone_lib.cli.init_logger a bad filename/data into fileConfig."""
+
+        with mock.patch("pkg_resources.resource_stream") as MockClass:
+            instance = MockClass.return_value
+            instance.name = '/tmp/XXX1234'
+            with self.assertRaises(KeyError):
+                cli.init_logging("foo.bar", "baz.conf")
+
+
+if __name__ == '__main__':
+    unittest.main()
