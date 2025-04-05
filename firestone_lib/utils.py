d174403652368e72c13d02334889e9d3ff2b98ad
Erick Bourgeois <erick@jeb.ca>
2025-04-05 14:19:13 -0400

    Add new function: split_capitalize that splits a string and capitalizes each "word"
    Signed-off-by: Erick Bourgeois <erick@jeb.ca>

--- firestone_lib/utils.py
+++ firestone_lib/utils.py
@@ -1,10 +1,12 @@
 """
-Utility module for firestone project.
+Utility module for firestone project and anyone else wanting to use it!
 """
 
 import functools
 
 import asyncio
+
+DEFAULT_SEPARATOR = "_"
 
 
 def click_coro(func):
@@ -15,3 +17,17 @@ def click_coro(func):
         return loop.run_until_complete(func(*args, **kwargs))
 
     return functools.update_wrapper(wrapper, func)
+
+
+def split_capitalize(input_str: str, sep: str = None) -> str:
+    """Split a string on `sep` and capitalize the words."""
+    if sep is None:
+        sep = DEFAULT_SEPARATOR
+
+    words = input_str.split(sep)
+
+    pretty_name = []
+    for word in words:
+        pretty_name.append(word.capitalize())
+
+    return " ".join(pretty_name)
